70602fa4358043a8c7923be030bef8a3522e66af
thinker3197 <ashishgupta.3197@gmail.com>
2016-07-03 18:47:48 +0530

    set route

--- examples/todo-app/app.js
+++ examples/todo-app/app.js
@@ -1,4 +1,23 @@
-angular.module('myApp',[])
-		.controller(function(){
-			
-		})
+angular.module('myApp',['ngRoute'])
+		.config(function($routeProvider){
+			'use strict';
+
+			var routeConfig = {
+				controller: 'todoCtrl',
+				templateUrl: 'todoApp.html',
+				resolve: {
+					store: function(todoStorage) {
+						return todoStorage.then(function(module){
+							module.get();
+							return module;
+						});
+					}
+				}
+			};
+
+			$routeProvider.when('/',routeConfig)
+						  .when('/:status',routeConfig)
+						  .otherwise({
+						  	redirectTo: '/'
+						  });
+		});
